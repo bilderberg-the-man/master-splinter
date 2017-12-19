0c9591fa6dfc9881fc9dfe7a20466c205d2296fd
Bilderberg <bilderberg@fuckjava.com>
2017-12-19 17:55:46 -0600

    update to splitArrayBuffer

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const uuidv4 = require('uuid/v4')
+import * as SparkMD5 from 'spark-md5'
 
 interface FileReaderEvent extends Event {
   target: any
@@ -39,31 +39,36 @@ export default {
    * @param buff ArrayBuffer
    * @param size number
    */
-  splitArrayBuffer (buff: ArrayBuffer, size: number = 256000): Promise<Chunk[]> {
+  splitArrayBuffer (buff: ArrayBuffer, size: number = 256000): Promise<object> {
     return new Promise((resolve, _) => {
-      let chunks: Chunk[] = []
+      let hm: any = {}
+      let sliceNum = 0
 
-      while (chunks.length < buff.byteLength / size) {
-        let start = chunks.length * size
+      while (sliceNum < buff.byteLength / size) {
+        let start = sliceNum * size
         let end = start + size
         let chunk = buff.slice(start, end)
-        let previous = chunks[chunks.length - 1]
 
-        chunks.push({
-          id: uuidv4(),
-          data: chunk,
-          link: previous ? previous.id : null
-        })
+        hm[hash(chunk)] = chunk
+
+        sliceNum ++
       }
 
-      resolve(chunks)
+      resolve(hm)
     })
   },
 
   combineChunks (chunks: Chunk[]): Promise<ArrayBuffer> {
     return new Promise((resolve, _) => {
-      console.log(chunks.reverse)
+      console.log(chunks)
       resolve(new ArrayBuffer(1024))
     })
   }
+}
+
+// hash the given array buffer in order to create an identifier for a given
+// chunk. The goal is that a hash map will hold the given data and if 2
+// chunks are identical they should only represented once in the HM
+function hash (chunk: ArrayBuffer): string {
+  return SparkMD5.ArrayBuffer.hash(chunk, false)
 }
