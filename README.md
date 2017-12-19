8baa28f1564462b47172cdc17a40c05241c9f1cc
Bilderberg <bilderberg@fuckjava.com>
2017-12-19 16:49:12 -0600

    splitting files

--- README.md
+++ README.md
@@ -1,28 +1,5 @@
-# Bilderberg Starter Pack
+# Master Splinter
 
-This repository has everything I need to start a project.
+Split files into smaller chunks
 
-Includes:
-
-- [browserify](https://www.npmjs.com/package/browserify)
-- [tsify](https://www.npmjs.com/package/tsify)
-- [tape](https://www.npmjs.com/package/tape)
-- [tape-run](https://www.npmjs.com/package/tape-run)
-- [tap-spec](https://www.npmjs.com/package/tap-spec)
-- [tslint](https://www.npmjs.com/package/tslint)
-- [tslint-config-standard](https://www.npmjs.com/package/tslint-config-standard)
-- [typescript](https://www.npmjs.com/package/typescript)
-
-## Set Up
-```bash
-## Clone repository
-$ git clone git@github.com:bilderberg-the-man/starter-pack.git project-name
-$ cd project-name
-
-## Create a new project on github
-$ git remote remove origin
-$ git remote add origin git@github.com:bilderberg-the-man/project-name
-
-## Install the dependencies
-$ yarn install
-```
+![master splinter](https://static.comicvine.com/uploads/original/4/49448/5635221-master+splinter+2.jpg)
--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,35 @@
-const test = require('tape')
+import Splinter from '../src/index'
+const test = require('blue-tape')
 
-test('it equals true', (t: any) => {
-  t.ok(true)
-  t.end()
+test('it returns an ArrayBuffer given that it has been handed a blob', (t: any) => {
+  let file = new Blob(['This is a test file'], { type: 'plain/text' })
+
+  return Splinter.convertFileToArrayBuffer(file)
+    .then(result => {
+      t.ok(result instanceof ArrayBuffer)
+      t.equals(result.byteLength, 19)
+    })
+})
+
+test('it splits an ArrayBuffer to tiny little pieces', (t: any) => {
+  const ab = new ArrayBuffer(1024 * 1024 * 10) // 1Mb
+  const size = 256000
+
+  return Splinter.splitArrayBuffer(ab, size)
+    .then(result => {
+      t.equals(result.length, Math.ceil(ab.byteLength / size))
+    })
+})
+
+test('it reassembles the file from the peices', (t: any) => {
+  const ab = new ArrayBuffer(1024 * 1024 * 10) // 1Mb
+
+  return Splinter.splitArrayBuffer(ab, 256000)
+    .then(chunks => {
+      return Splinter.combineChunks(chunks)
+    })
+    .then(reconstructedAB => {
+      console.log(reconstructedAB)
+      t.ok(true)
+    })
 })
